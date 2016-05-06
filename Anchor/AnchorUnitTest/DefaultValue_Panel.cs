bec06e1672ef499d21ffac1ac57bf32d8c249633
AlexNoskov <alexnoskov1@hotmail.com>
2016-05-06 23:05:29 +0300

    UnitTest.Panel. Добавлено. Проверка значения по умолчанию.

---
+++ Anchor/AnchorUnitTest/DefaultValue_Panel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnchorUnitTest
+{
+    public class DefaultValue_Panel<T>
+    {
+        public static void IsDefault(T value)
+        {
+            if (default(T) == null)
+            {
+                Assert.IsNull(value);
+            }
+            else
+            {
+                Assert.IsTrue(value.Equals(default(T)));
+            }
+        }
+        public static void IsNotDefault(T value)
+        {
+            if (default(T) == null)
+            {
+                Assert.IsNotNull(value);
+            }
+            else
+            {
+                Assert.IsFalse(value.Equals(default(T)));
+            }
+        }
+    }
+}
