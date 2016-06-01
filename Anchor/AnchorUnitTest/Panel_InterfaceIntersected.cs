5a4c52cc6cf6d1c1cfe0e9d4f32e0ea2779bb431
AlexNoskov <alexnoskov1@hotmail.com>
2016-06-02 00:35:19 +0300

    Модульные тесты. Добавлено. Панели тестирования интерфейсов отрезок и пересечение.

---
+++ Anchor/AnchorUnitTest/Panel_InterfaceIntersected.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Anchor;
+namespace AnchorUnitTest
+{
+    /// <summary>
+    /// Панель тестирования интерфейса сервиса, определяющего факт пересечения.
+    /// </summary>    
+    public class Panel_InterfaceIntersected<TLabel, TSpan>
+    {
+        public static void LabelLessStart(IIntersected<TLabel, TSpan> intersected, TLabel label)
+        {
+            Assert.Inconclusive();
+        }
+
+        public static void LabelMoreEnd(IIntersected<TLabel, TSpan> intersected, TLabel label)
+        {
+            Assert.Inconclusive();
+        }
+
+        public static void LabelOn(IIntersected<TLabel, TSpan> intersected)
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
---
+++ Anchor/AnchorUnitTest/Panel_InterfaceSegment.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Anchor;
+
+namespace AnchorUnitTest
+{
+    /// <summary>
+    /// Панель тестирования интерфейса, представляющего отрезок.
+    /// </summary>
+    public class Panel_InterfaceSegment<TLabel, TSpan>
+    {       
+        public static void StartState_Empty (ISegment<TLabel, TSpan> segment)
+        {
+            Panel_DefaultValue<TLabel>.IsDefault(segment.Start);
+            Panel_DefaultValue<TLabel>.IsDefault(segment.End);
+            Assert.IsTrue(segment.IsPoint);
+        }
+
+        public static void SetSegment_ReverseLabel(ISegment<TLabel, TSpan> segment, TLabel startMoreEnd, TLabel endLowerStart)
+        {
+            segment.SetSegment(startMoreEnd, endLowerStart);            
+            Assert.IsTrue(segment.Start.Equals(startMoreEnd));
+            Assert.IsTrue(segment.End.Equals(startMoreEnd));
+        }
+
+        public static void SetSegment(ISegment<TLabel, TSpan> segment, TLabel start, TLabel end)
+        {
+            segment.SetSegment(start, end);
+            Assert.IsTrue(segment.Start.Equals(start));
+            Assert.IsTrue(segment.End.Equals(end));
+        }
+
+        public static void IsPoint(ISegment<TLabel, TSpan> segment, TLabel start, TLabel end)
+        {
+            segment.SetSegment(start, end);
+            Assert.IsFalse(segment.IsPoint);
+            segment.SetSegment(start, start);
+            Assert.IsTrue(segment.IsPoint);
+        }
+
+        public static void SetSpan(ISegment<TLabel, TSpan> segment, TSpan span, TLabel etalonEnd)
+        {
+            segment.SetSpan(span);
+            Assert.IsTrue(segment.End.Equals(etalonEnd));
+        }
+    }
+}
